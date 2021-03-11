4afd14668629369452cc323bba201c1f4947d203
吉村侯太朗 <youwatanabeoku@yahoo.co.jp>
2021-03-11 15:19:33 +0000

    Update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   def index
+   @books = Book.all
   end
 
   def new
@@ -7,11 +8,20 @@ class BooksController < ApplicationController
   end
 
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def book_params
+    params.permit(:title,:body)
   end
 end
