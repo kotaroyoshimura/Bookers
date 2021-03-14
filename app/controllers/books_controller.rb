6c4ec3203f9db60f4166979192b8ffb73da03484
吉村侯太朗 <youwatanabeoku@yahoo.co.jp>
2021-03-14 04:54:27 +0000

    Update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,17 @@
 class BooksController < ApplicationController
   def index
    @books = Book.all
+   @book = Book.new
   end
 
-  def new
-    @book = Book.new
-  end
 
   def create
     book = Book.new(book_params)
     if book.save
-      flash[:create_data] = "Successfully"
+      flash[:create_data] = "Book was successfully created."
       redirect_to book_path(book.id)
+    else
+      redirect_to books_path
     end
   end
 
@@ -25,19 +25,23 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:update_data] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    if book.destroy
+      flash[:destroy_data] = "Book was successfully destroy"
+      redirect_to books_path
+    end
   end
 
   private
 
   def book_params
-    params.permit(:title,:body)
+    params.require(:book).permit(:title,:body)
   end
 end
